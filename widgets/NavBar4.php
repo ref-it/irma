4d0f8770786164d6aee781922f4a7cb669a54e63
Lukas Staab <l.staab@posteo.de>
2022-01-21 05:05:30 +0100

    show major role next to profile

--- widgets/NavBar4.php
+++ widgets/NavBar4.php
@@ -3,12 +3,12 @@
 
 namespace app\widgets;
 
+use app\models\db\User;
 use rmrevin\yii\fontawesome\FAS;
 use Yii;
 use yii\bootstrap4\Nav;
 use yii\bootstrap4\NavBar;
 use yii\helpers\Html;
-use yii\web\User;
 
 
 /**
@@ -30,16 +30,52 @@ class NavBar4 extends \yii\bootstrap4\Widget
         ]);
 
 
-        /** @var User $user */
-        $isLoggedIn = !Yii::$app->user->isGuest;
-        /** @var User $id */
-        $id = Yii::$app->user->getIdentity();
+        $user = Yii::$app->user;
+        $isLoggedIn = !$user->isGuest;
+        /* @var User $id */
+        $id = $user->getIdentity();
         $nameTag = $id->fullName ?? $id->username ?? 'Anonymous?!';
-
+        echo Nav::widget([
+            'options' => ['class' => 'navbar-nav mr-auto'],
+            'items' => [
+                [
+                    'label' => FAS::icon('sitemap') . ' Gremien',
+                    'url' => ['gremien/'],
+                    'visible' => $isLoggedIn,
+                    'encode' => false,
+                ],
+                [
+                    'label' => FAS::icon('users') . ' Personen',
+                    'url' => ['user/'],
+                    'visible' => $isLoggedIn,
+                    'encode' => false,
+                ],
+            ]
+        ]);
+        $roleLabel = 'unset';
+        if($isLoggedIn){
+            // parse the mayor role of the user
+            $adminRealms = $id->adminRealms;
+            if(!empty($adminRealms)){
+                $adminRealms = array_map(static fn($realm) => $realm->uid, $adminRealms);
+                if(in_array('oa', $adminRealms, true)){
+                    $roleLabel = FAS::icon('dragon') . ' Superadmin';
+                }else{
+                    $roleLabel = FAS::icon('user-cog') . ' Admin: ' . implode(', ' , $adminRealms);
+                }
+            }else{
+                $memberRealms = $id->realms;
+                if(!empty($memberRealms)){
+                    $memberRealms = array_map(static fn($realm) => $realm->uid, $memberRealms);
+                    $roleLabel = FAS::icon('user-tag') . ' Member: ' . implode(', ' , $adminRealms);
+                }
+            }
+        }
         // align on the right side with ml-auto
         echo Nav::widget([
             'options' => ['class' => 'navbar-nav ml-auto'],
             'items' => [
+
                 [
                     'label' => FAS::icon('sign-in-alt') . ' Login',
                     'url' => ['auth/wayfinder'],
@@ -51,6 +87,13 @@ class NavBar4 extends \yii\bootstrap4\Widget
                     'url' => ['auth/register'],
                     'visible' => !$isLoggedIn,
                     'encode' => false,
+                ],
+                [
+                    'label' => $roleLabel,
+                    'visible' => $isLoggedIn,
+                    'encode' => false,
+                    'active' => true,
+                    //'disabled' => true,
                 ],
                 [
                     'label' => Html::img(Yii::$app->request->baseUrl . '/img/dummyPerson.svg', ['height' => 30, 'class' => 'pr-1']) . ' ' . $nameTag,
