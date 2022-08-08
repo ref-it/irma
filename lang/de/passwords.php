edeb450282ec8906e976aab1070c53c8b21faf89
Jennifer Graul <me@netali.de>
2022-08-08 14:49:38 +0200

    lang: begin i18n

---
+++ lang/de/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Dein Passwort wurde zurückgesetzt!',
+    'sent' => 'Wir haben dir einen Passwort Reset Link per Mail zugesendet!',
+    'throttled' => 'Bitte warte etwas bis zu deinem nächten Versuch.',
+    'token' => 'Dieses Passwort Reset Token ist ungültig.',
+    'user' => "Wir können keinen Benutzer mit der angegebenen Mailadresse finden.",
+
+];
