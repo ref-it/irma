4fe117c5109f83a3afa3b724e7f31303edd148e2
Jennifer Graul <me@netali.de>
2022-11-10 17:10:40 +0100

    add danger button component

---
+++ resources/views/components/button/danger.blade.php
@@ -0,0 +1,11 @@
+{{--
+-- Important note:
+--
+-- This template is based on an example from Tailwind UI, and is used here with permission from Tailwind Labs
+-- for educational purposes only. Please do not use this template in your own projects without purchasing a
+-- Tailwind UI license, or they’ll have to tighten up the licensing and you’ll ruin the fun for everyone.
+--
+-- Purchase here: https://tailwindui.com/
+--}}
+
+<x-button {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-500 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500']) }}>{{ $slot }}</x-button>
