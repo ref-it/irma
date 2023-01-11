93324a9878d909b588a94cf52c02d2239be8e6c2
Jennifer Graul <me@netali.de>
2023-01-11 20:29:34 +0100

    role-user-relation: remove uniqueness of relation

--- database/migrations/2022_11_03_161753_create_role_user_relation.php
+++ database/migrations/2022_11_03_161753_create_role_user_relation.php
@@ -14,12 +14,12 @@ return new class extends Migration
     public function up()
     {
         Schema::create('role_user_relation', function (Blueprint $table) {
+            $table->id();
             $table->bigInteger('role_id')->unsigned();
             $table->bigInteger('user_id')->unsigned();
             $table->date('from');
             $table->date('until')->nullable();
             $table->timestamps();
-            $table->primary(['role_id', 'user_id']);
         });
     }
 
